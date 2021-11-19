83e54e93138d383b8bbe30128a2546ba3cec70a2
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2021-11-19 20:13:33 +0000

    CSS Updates

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,27 @@ import React from "react";
 function Header() {
   return (
     <header className="header">
-      <p className="header-title">High Scores Table</p>
+      <div class="logo-wrapper">
+        <img
+          src="https://i.ibb.co/zXCP3jj/earth.png"
+          class="logo"
+          alt="Logo"
+        ></img>
+      </div>
+
+      <div class="header-title-wrapper">
+        <p className="header-title">High Score Tables</p>
+      </div>
     </header>
   );
+}
+
+{
+  /* <a href="https://imgbb.com/"><img src="https://i.ibb.co/zXCP3jj/earth.png" alt="earth" border="0"></a> */
+}
+
+{
+  /* <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */
 }
 
 export default Header;
